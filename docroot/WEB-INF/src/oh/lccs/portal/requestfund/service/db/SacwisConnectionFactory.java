baaa89ae0353d94ad3a85b0f854fa36ee5b328c6
santoshvellore <santosh.k.vellore@gmail.com>
2014-07-31 10:33:13 -0400

    Checked in as part of externalising jdbc properties

--- docroot/WEB-INF/src/oh/lccs/portal/requestfund/service/db/SacwisConnectionFactory.java
+++ docroot/WEB-INF/src/oh/lccs/portal/requestfund/service/db/SacwisConnectionFactory.java
@@ -1,7 +1,10 @@
 package oh.lccs.portal.requestfund.service.db;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -21,14 +24,20 @@ public class SacwisConnectionFactory {
 	private static SqlSessionFactory loadMapper(){
 	    try{
 	    	 InputStream inputStream = null;
+	    	 Properties properties = new Properties();
+	    	 
 				try {
 					inputStream = Resources.getResourceAsStream("oh/lccs/portal/requestfund/service/db/config/sacwis-mybatis-configuration.xml");
+					File configDir = new File(System.getProperty("catalina.base"), "conf");
+					File configFile = new File(configDir, "sacwis-jdbc.properties");
+					InputStream stream = new FileInputStream(configFile);
+					properties.load(stream);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				
-	    	 return new SqlSessionFactoryBuilder().build(inputStream);
+	    	 return new SqlSessionFactoryBuilder().build(inputStream,properties);
        }catch(Exception e){
            //TODO: Need to handle it.
     	   e.printStackTrace();
